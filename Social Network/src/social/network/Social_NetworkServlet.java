5118b0339b2d5bf0dc38f03147990b4d4a0b8814
Swagg3r <aurelm38@gmail.com>
2016-03-24 20:00:07 +0100

    Ajout fichiers de base
    Ajout des fichiers de base pour le projet cloud en IBD.

---
+++ Social Network/src/social/network/Social_NetworkServlet.java
@@ -0,0 +1,13 @@
+package social.network;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Social_NetworkServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
