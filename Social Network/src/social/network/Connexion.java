2ac15619f858b349a99af0492cf7904cb2075bca
Aurelien <aurelm38@gmail.com>
2016-04-01 15:02:46 +0200

    modifs sur connexion (non fini)

--- Social Network/src/social/network/Connexion.java
+++ Social Network/src/social/network/Connexion.java
@@ -1,7 +1,8 @@
 package social.network;
 
 import java.io.IOException;
-
+import java.util.List;
+import static com.googlecode.objectify.ObjectifyService.ofy;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,9 @@ public class Connexion extends HttpServlet {
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		System.out.println("Je suis dans la servlet Connexion");
+		String addrMail = req.getParameter("mail");
+		String pw = req.getParameter("pw");
+		List<Personne> personnes = ofy().load().type(Personne.class).filter("mail ==", addrMail).list();
 		if (/*Requete dans bdd avec login + mdp ET si correct alors */true) {
 			//Variable de session ...
 			
