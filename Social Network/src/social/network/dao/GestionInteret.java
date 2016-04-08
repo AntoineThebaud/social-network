db981177e9553780c3ec4b5388e521a0e64582f9
AntoineThebaud <antoine.thebaud@yahoo.fr>
2016-04-08 13:38:47 +0200

    resolve fucking conflict + push interets

--- Social Network/src/social/network/dao/GestionInteret.java
+++ Social Network/src/social/network/dao/GestionInteret.java
@@ -1,11 +1,22 @@
 package social.network.dao;
 
-import social.network.Personne; 
+import com.googlecode.objectify.ObjectifyService;
+
 import social.network.Interet; 
 
- 
 public class GestionInteret extends Dao<Interet> { 
     public GestionInteret() {
         super(Interet.class); 
+    }
+    
+    public boolean exists(String interet) {
+    	//return ObjectifyService.ofy().load().key(Key<Interet> interet).now().count()==0;
+    		
+    	
+    	//		ObjectifyService.
+    			
+    			//.filterKey(thingKey).count() != 0;
+    	//return query().filterKey("nom", interet).first().now(); 
+    	return false;
     } 
 }
